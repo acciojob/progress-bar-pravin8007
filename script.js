88b6736c9bf94dff4088b02ddbe8766168327829
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2024-01-07 11:35:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 //your JS code here. If required.
+const circles = document.querySelectorAll('.circle');
+  const lines = document.querySelectorAll('.line');
+  const prevButton = document.getElementById('prev');
+  const nextButton = document.getElementById('next');
+  let currentCircle = 1;
+
+  function updateButtons() {
+    prevButton.disabled = currentCircle === 1;
+    nextButton.disabled = currentCircle === circles.length;
+  }
+
+  function updateProgress() {
+    circles.forEach((circle, index) => {
+      if (index < currentCircle) {
+        circle.classList.add('active');
+      } else {
+        circle.classList.remove('active');
+      }
+    });
+
+	  lines.forEach((line, index) => {
+      if (index < currentCircle - 1) {
+        line.classList.add('active-line');
+      } else {
+        line.classList.remove('active-line');
+      }
+    });
+	  
+  }
+
+  function nextCircle() {
+    if (currentCircle < circles.length) {
+      currentCircle++;
+      updateProgress();
+      updateButtons();
+    }
+  }
+
+  function prevCircle() {
+    if (currentCircle > 1) {
+      currentCircle--;
+      updateProgress();
+      updateButtons();
+    }
+  }
